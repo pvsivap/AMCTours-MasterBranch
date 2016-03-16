b5ad9d2e62ba086c97097df968b26e53147a3522
pvsivap <grt.sivaprasad@gmail.com>
2016-03-16 19:18:14 +0530

    First cut.

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblIntNatTour = new System.Windows.Forms.LinkLabel();
+            this.LblDomasticTour = new System.Windows.Forms.LinkLabel();
+            this.pnlTour = new System.Windows.Forms.Panel();
+            this.lblTourName6 = new System.Windows.Forms.LinkLabel();
+            this.lblTourName5 = new System.Windows.Forms.LinkLabel();
+            this.lblTourName4 = new System.Windows.Forms.LinkLabel();
+            this.lblTourName3 = new System.Windows.Forms.LinkLabel();
+            this.lblTourName2 = new System.Windows.Forms.LinkLabel();
+            this.lblTourName1 = new System.Windows.Forms.LinkLabel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.pctTour = new System.Windows.Forms.PictureBox();
+            this.btBooking = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.pnlTour.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pctTour)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblIntNatTour);
+            this.panel1.Controls.Add(this.LblDomasticTour);
+            this.panel1.Location = new System.Drawing.Point(3, 5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(164, 89);
+            this.panel1.TabIndex = 0;
+            // 
+            // lblIntNatTour
+            // 
+            this.lblIntNatTour.AutoSize = true;
+            this.lblIntNatTour.Location = new System.Drawing.Point(9, 42);
+            this.lblIntNatTour.Name = "lblIntNatTour";
+            this.lblIntNatTour.Size = new System.Drawing.Size(91, 13);
+            this.lblIntNatTour.TabIndex = 1;
+            this.lblIntNatTour.TabStop = true;
+            this.lblIntNatTour.Text = "International tours";
+            this.lblIntNatTour.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lblIntNatTour_LinkClicked);
+            // 
+            // LblDomasticTour
+            // 
+            this.LblDomasticTour.AutoSize = true;
+            this.LblDomasticTour.Location = new System.Drawing.Point(7, 17);
+            this.LblDomasticTour.Name = "LblDomasticTour";
+            this.LblDomasticTour.Size = new System.Drawing.Size(77, 13);
+            this.LblDomasticTour.TabIndex = 0;
+            this.LblDomasticTour.TabStop = true;
+            this.LblDomasticTour.Text = "Domastic toors";
+            this.LblDomasticTour.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LblDomasticTour_LinkClicked);
+            // 
+            // pnlTour
+            // 
+            this.pnlTour.Controls.Add(this.lblTourName6);
+            this.pnlTour.Controls.Add(this.lblTourName5);
+            this.pnlTour.Controls.Add(this.lblTourName4);
+            this.pnlTour.Controls.Add(this.lblTourName3);
+            this.pnlTour.Controls.Add(this.lblTourName2);
+            this.pnlTour.Controls.Add(this.lblTourName1);
+            this.pnlTour.Location = new System.Drawing.Point(5, 123);
+            this.pnlTour.Name = "pnlTour";
+            this.pnlTour.Size = new System.Drawing.Size(162, 332);
+            this.pnlTour.TabIndex = 1;
+            // 
+            // lblTourName6
+            // 
+            this.lblTourName6.AutoSize = true;
+            this.lblTourName6.Location = new System.Drawing.Point(14, 163);
+            this.lblTourName6.Name = "lblTourName6";
+            this.lblTourName6.Size = new System.Drawing.Size(55, 13);
+            this.lblTourName6.TabIndex = 5;
+            this.lblTourName6.TabStop = true;
+            this.lblTourName6.Text = "linkLabel6";
+            this.lblTourName6.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lblTourName6_LinkClicked);
+            // 
+            // lblTourName5
+            // 
+            this.lblTourName5.AutoSize = true;
+            this.lblTourName5.Location = new System.Drawing.Point(13, 130);
+            this.lblTourName5.Name = "lblTourName5";
+            this.lblTourName5.Size = new System.Drawing.Size(55, 13);
+            this.lblTourName5.TabIndex = 4;
+            this.lblTourName5.TabStop = true;
+            this.lblTourName5.Text = "linkLabel5";
+            this.lblTourName5.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lblTourName5_LinkClicked);
+            // 
+            // lblTourName4
+            // 
+            this.lblTourName4.AutoSize = true;
+            this.lblTourName4.Location = new System.Drawing.Point(11, 99);
+            this.lblTourName4.Name = "lblTourName4";
+            this.lblTourName4.Size = new System.Drawing.Size(55, 13);
+            this.lblTourName4.TabIndex = 3;
+            this.lblTourName4.TabStop = true;
+            this.lblTourName4.Text = "linkLabel4";
+            this.lblTourName4.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lblTourName4_LinkClicked);
+            // 
+            // lblTourName3
+            // 
+            this.lblTourName3.AutoSize = true;
+            this.lblTourName3.Location = new System.Drawing.Point(12, 69);
+            this.lblTourName3.Name = "lblTourName3";
+            this.lblTourName3.Size = new System.Drawing.Size(55, 13);
+            this.lblTourName3.TabIndex = 2;
+            this.lblTourName3.TabStop = true;
+            this.lblTourName3.Text = "linkLabel3";
+            this.lblTourName3.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lblTourName3_LinkClicked);
+            // 
+            // lblTourName2
+            // 
+            this.lblTourName2.AutoSize = true;
+            this.lblTourName2.Location = new System.Drawing.Point(12, 42);
+            this.lblTourName2.Name = "lblTourName2";
+            this.lblTourName2.Size = new System.Drawing.Size(55, 13);
+            this.lblTourName2.TabIndex = 1;
+            this.lblTourName2.TabStop = true;
+            this.lblTourName2.Text = "linkLabel2";
+            this.lblTourName2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lblTourName2_LinkClicked);
+            // 
+            // lblTourName1
+            // 
+            this.lblTourName1.AutoSize = true;
+            this.lblTourName1.Location = new System.Drawing.Point(11, 16);
+            this.lblTourName1.Name = "lblTourName1";
+            this.lblTourName1.Size = new System.Drawing.Size(55, 13);
+            this.lblTourName1.TabIndex = 0;
+            this.lblTourName1.TabStop = true;
+            this.lblTourName1.Text = "linkLabel1";
+            this.lblTourName1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lblTourName1_LinkClicked);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.pctTour);
+            this.panel3.Location = new System.Drawing.Point(174, 8);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(776, 447);
+            this.panel3.TabIndex = 2;
+            // 
+            // pctTour
+            // 
+            this.pctTour.Location = new System.Drawing.Point(3, 4);
+            this.pctTour.Name = "pctTour";
+            this.pctTour.Size = new System.Drawing.Size(763, 388);
+            this.pctTour.TabIndex = 0;
+            this.pctTour.TabStop = false;
+            this.pctTour.Click += new System.EventHandler(this.pctTour_Click);
+            // 
+            // btBooking
+            // 
+            this.btBooking.Location = new System.Drawing.Point(798, 406);
+            this.btBooking.Name = "btBooking";
+            this.btBooking.Size = new System.Drawing.Size(133, 30);
+            this.btBooking.TabIndex = 1;
+            this.btBooking.Text = "Proceed for booking";
+            this.btBooking.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(952, 467);
+            this.Controls.Add(this.btBooking);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.pnlTour);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Text = "AMC Tours and travels";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.pnlTour.ResumeLayout(false);
+            this.pnlTour.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pctTour)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.LinkLabel LblDomasticTour;
+        private System.Windows.Forms.LinkLabel lblIntNatTour;
+        private System.Windows.Forms.Panel pnlTour;
+        private System.Windows.Forms.LinkLabel lblTourName6;
+        private System.Windows.Forms.LinkLabel lblTourName5;
+        private System.Windows.Forms.LinkLabel lblTourName4;
+        private System.Windows.Forms.LinkLabel lblTourName3;
+        private System.Windows.Forms.LinkLabel lblTourName2;
+        private System.Windows.Forms.LinkLabel lblTourName1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button btBooking;
+        private System.Windows.Forms.PictureBox pctTour;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        bool isIntTravel = false;
+        bool isDomTravel = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void LblDomasticTour_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            //lblTourName.BackColor = Color.Red;
+            //lblTourName1.ForeColor = Color.Blue;
+            lblTourName1.Text = "Delhi Tour-Test";
+            lblTourName1.Font = new Font("Georgia", 12);
+
+            //lblTourName2.BackColor = Color.Red;
+            //lblTourName2.ForeColor = Color.Blue;
+            lblTourName2.Text = "Agra Madhura Tour-Test";
+            lblTourName2.Font = new Font("Georgia", 12);
+
+            //lblTourName3.BackColor = Color.Red;
+            //lblTourName3.ForeColor = Color.Blue;
+            lblTourName3.Text = "Manali Tour-test";
+            lblTourName3.Font = new Font("Georgia", 12);
+
+            //lblTourName4.BackColor = Color.Red;
+            //lblTourName4.ForeColor = Color.Blue;
+            lblTourName4.Text = "Kashmir Tour";
+            lblTourName4.Font = new Font("Georgia", 12);
+
+            //lblTourName5.BackColor = Color.Red;
+            //lblTourName5.ForeColor = Color.Blue;
+            lblTourName5.Text = "Amarnath Tour";
+            lblTourName5.Font = new Font("Georgia", 12);
+
+            //lblTourName6.BackColor = Color.Red;
+            //lblTourName6.ForeColor = Color.Blue;
+            lblTourName6.Text = "Rishikesh Tour";
+            lblTourName6.Font = new Font("Georgia", 12);
+            //new comments are added
+            pnlTour.Visible = true;
+            isIntTravel = false;
+            isDomTravel = true;
+        }
+
+        private void lblIntNatTour_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            //lblTourName1.BackColor = Color.Red;
+            //lblTourName1.ForeColor = Color.Blue;
+            lblTourName1.Text = "Amarica Tour";
+            lblTourName1.Font = new Font("Georgia", 12);
+
+            //lblTourName2.BackColor = Color.Red;
+            //lblTourName2.ForeColor = Color.Blue;
+            lblTourName2.Text = "Africa Tour";
+            lblTourName2.Font = new Font("Georgia", 12);
+
+            //lblTourName3.BackColor = Color.Red;
+            //lblTourName3.ForeColor = Color.Blue;
+            lblTourName3.Text = "Australia Tour";
+            lblTourName3.Font = new Font("Georgia", 12);
+
+            //lblTourName4.BackColor = Color.Red;
+            //lblTourName4.ForeColor = Color.Blue;
+            lblTourName4.Text = "China Tour";
+            lblTourName4.Font = new Font("Georgia", 12);
+
+            //lblTourName5.BackColor = Color.Red;
+            //lblTourName5.ForeColor = Color.Blue;
+            lblTourName5.Text = "Greece Tour";
+            lblTourName5.Font = new Font("Georgia", 12);
+
+           // lblTourName6.BackColor = Color.Red;
+           // lblTourName6.ForeColor = Color.Blue;
+            lblTourName6.Text = "Swiss Tour";
+            lblTourName6.Font = new Font("Georgia", 12);
+
+            pnlTour.Visible = true;
+            isIntTravel = true;
+            isDomTravel = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pnlTour.Visible = false;
+            pctTour.Visible = false;
+            btBooking.Visible = false;
+
+            //LblDomasticTour.BackColor = Color.Red;
+            //lblIntNatTour.BackColor = Color.Red;
+            
+            LblDomasticTour.Font = new Font("Georgia", 14);
+            lblIntNatTour.Font = new Font("Georgia", 14);
+        }
+
+        private void lblTourName1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            if ((lblTourName1.Text == "Delhi Tour-Test") && (isDomTravel))
+                pctTour.Image = Image.FromFile("D:\\Siva\\Tours\\DelhiTour.png");
+                //pctTour.Image = Image.FromFile("\\Tours\\DelhiTour.png");
+            else
+                pctTour.Image = Image.FromFile("D:\\Siva\\Tours\\America.png");
+                //pctTour.Image = Image.FromFile("\\Tours\\America.png");
+
+            pctTour.SizeMode = PictureBoxSizeMode.CenterImage;
+            pctTour.SizeMode = PictureBoxSizeMode.AutoSize;
+            pctTour.Visible = true;
+            btBooking.Visible = true;
+
+        }
+
+        private void lblTourName2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            if ((lblTourName2.Text == "Agra Madhura Tour-Test") && (isDomTravel))
+                pctTour.Image = Image.FromFile("D:\\Siva\\Tours\\Agra-Mathura.png");
+            else
+                pctTour.Image = Image.FromFile("D:\\Siva\\Tours\\Africa.png");
+
+            pctTour.SizeMode = PictureBoxSizeMode.CenterImage;
+            pctTour.SizeMode = PictureBoxSizeMode.AutoSize;
+            pctTour.Visible = true;
+            btBooking.Visible = true;
+        }
+
+        private void lblTourName3_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            if ((lblTourName3.Text == "Manali Tour-test") && (isDomTravel))
+                pctTour.Image = Image.FromFile("D:\\Siva\\Tours\\Simla.png");
+            else
+                pctTour.Image = Image.FromFile("D:\\Siva\\Tours\\Australia.png");
+
+            pctTour.SizeMode = PictureBoxSizeMode.CenterImage;
+            pctTour.SizeMode = PictureBoxSizeMode.AutoSize;
+            pctTour.Visible = true;
+            btBooking.Visible = true;
+        }
+
+        private void lblTourName4_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            if ((lblTourName4.Text == "Kashmir Tour") && (isDomTravel))
+                pctTour.Image = Image.FromFile("D:\\Siva\\Tours\\Kashmir.png");
+            else
+                pctTour.Image = Image.FromFile("D:\\Siva\\Tours\\China.png");
+
+            pctTour.SizeMode = PictureBoxSizeMode.CenterImage;
+            pctTour.SizeMode = PictureBoxSizeMode.AutoSize;
+            pctTour.Visible = true;
+            btBooking.Visible = true;
+        }
+
+        private void lblTourName5_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            if ((lblTourName5.Text == "Amarnath Tour") && (isDomTravel))
+                pctTour.Image = Image.FromFile("D:\\Siva\\Tours\\Amarnath.png");
+            else
+                pctTour.Image = Image.FromFile("D:\\Siva\\Tours\\Greese.png");
+
+            pctTour.SizeMode = PictureBoxSizeMode.CenterImage;
+            pctTour.SizeMode = PictureBoxSizeMode.AutoSize;
+            pctTour.Visible = true;
+            btBooking.Visible = true;
+        }
+
+        private void lblTourName6_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            if ((lblTourName5.Text == "Rishikesh Tour") && (isDomTravel))
+                pctTour.Image = Image.FromFile("D:\\Siva\\Tours\\Rshikesh.png");
+            else
+                pctTour.Image = Image.FromFile("D:\\Siva\\Tours\\Swiss.png");
+
+            pctTour.SizeMode = PictureBoxSizeMode.CenterImage;
+            pctTour.SizeMode = PictureBoxSizeMode.AutoSize;
+            pctTour.Visible = true;
+            btBooking.Visible = true;
+        }
+
+        private void pctTour_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
